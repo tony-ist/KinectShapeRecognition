222ac42555df1e546e9303f1511e5e6955c7f335
ReFruity <istomanton@gmail.com>
2017-01-08 02:09:35 +0500

    add start button

--- KinectShapeRecognition/MainWindow.xaml.cs
+++ KinectShapeRecognition/MainWindow.xaml.cs
@@ -15,14 +15,16 @@ using System.Windows.Shapes;
 
 namespace KinectShapeRecognition
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
