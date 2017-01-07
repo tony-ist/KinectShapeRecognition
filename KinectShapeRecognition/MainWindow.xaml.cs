5d1207ca2774d3c4df9945fc862414eab0fd7bd0
ReFruity <istomanton@gmail.com>
2017-01-08 04:35:08 +0500

    display depth map

--- KinectShapeRecognition/MainWindow.xaml.cs
+++ KinectShapeRecognition/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,10 +14,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Microsoft.Kinect;
+
 namespace KinectShapeRecognition
 {
     public partial class MainWindow : Window
     {
+        private static readonly int pixelWidth = 320;
+        private static readonly int pixelHeight = 240;
+        private static readonly int bytesPerPixel = 2;
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -24,7 +31,22 @@ namespace KinectShapeRecognition
 
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
+            String textData = File.ReadAllText(@"data\air_pen_0.txt");
+            var depthArray = textData.Split(',').Where(s => !String.IsNullOrEmpty(s)).Select(short.Parse).ToArray();
+            DisplayDepthArray(depthArray);
+        }
 
+        private void DisplayDepthArray(short[] depthArray)
+        {
+            image.Source = BitmapSource.Create(
+                pixelWidth,
+                pixelHeight,
+                96, 96,
+                PixelFormats.Gray16,
+                null,
+                depthArray,
+                pixelWidth * bytesPerPixel
+            );
         }
     }
 }
