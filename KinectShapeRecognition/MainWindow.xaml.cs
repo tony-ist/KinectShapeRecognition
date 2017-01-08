e4d4770d85dbc3eacde1c9f9111b97c321f70e56
ReFruity <istomanton@gmail.com>
2017-01-08 23:30:31 +0500

    cleanup code

--- KinectShapeRecognition/MainWindow.xaml.cs
+++ KinectShapeRecognition/MainWindow.xaml.cs
@@ -31,13 +31,7 @@ namespace KinectShapeRecognition
         {
             String textData = File.ReadAllText(@"data\air_pen_0.txt");
             var depthArray = textData.Split(',').Where(s => !String.IsNullOrEmpty(s)).Select(short.Parse).ToArray();
-//            DisplayDepthArray(depthArray);
-//            DisplayDepthArray(Enumerable.Range(0, 320 * 240).Select(x => (short)x).ToArray());
-//            DisplayDepthArray(Enumerable.Repeat(17064, 320 * 240).Select(x => (short) x).ToArray());
-//            DisplayDepthArray(Enumerable.Repeat(-10000, 100).Concat(Enumerable.Repeat(0, 320*240 - 100)).ToArray());
-//            DisplayBgr(Enumerable.Range(0, 320*240).Select(i => HsvToBgr32(0, 0, 0)).ToArray());
-            DisplayBgr(Enumerable.Range(0, 320*240).Select(i => ~(i + 16000000)).ToArray());
-//            DisplayBgr(Enumerable.Repeat(0x0000ff, 320*240).ToArray());
+            DisplayDepthArray(depthArray);
         }
 
         private void DisplayDepthArray(short[] depthArray)
@@ -72,52 +66,6 @@ namespace KinectShapeRecognition
                 array,
                 pixelWidth*bytesPerPixel
                 );
-        }
-
-        private void TestHsvToBgr32()
-        {
-            
-        }
-
-        private static int HsvToBgr32(double hue, double saturation, double value)
-        {
-            int hi = Convert.ToInt32(Math.Floor(hue/60))%6;
-            double f = hue/60 - Math.Floor(hue/60);
-
-            value = value*255;
-            int v = Convert.ToInt32(value);
-            int p = Convert.ToInt32(value*(1 - saturation));
-            int q = Convert.ToInt32(value*(1 - f*saturation));
-            int t = Convert.ToInt32(value*(1 - (1 - f)*saturation));
-
-            switch (hi)
-            {
-                case 0:
-                    return v << 16 + t << 8 + p;
-                case 1:
-                    return q << 16 + v << 8 + p;
-                case 2:
-                    return p << 16 + v << 8 + t;
-                case 3:
-                    return p << 16 + q << 8 + v;
-                case 4:
-                    return t << 16 + p << 8 + v;
-                default:
-                    return v << 16 + p << 8 + q;
-            }
-
-//            if (hi == 0)
-//                return Color.FromArgb(255, v, t, p);
-//            else if (hi == 1)
-//                return Color.FromArgb(255, q, v, p);
-//            else if (hi == 2)
-//                return Color.FromArgb(255, p, v, t);
-//            else if (hi == 3)
-//                return Color.FromArgb(255, p, q, v);
-//            else if (hi == 4)
-//                return Color.FromArgb(255, t, p, v);
-//            else
-//                return Color.FromArgb(255, v, p, q);
         }
     }
 }
